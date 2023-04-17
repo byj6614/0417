fe04327feeb7f13a2eee79b848e3696dfb702476
현석 이 <inc02179@naver.com>
2023-04-17 20:28:16 +0900

    feat : 이현석 포트폴리오 아이디어
    포트폴리오 아이디어

---
+++ 0417/LeeIdea.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0417
+{
+    internal class LeeIdea
+    {
+        /*
+         * 포트폴리오 아이디어
+         * 
+         * 형사 캐릭터 생성
+         * 사망자의 신원 등을 조사를 통해 획득
+         * 획득한 자료로 범인을 특정
+         * 범인을 찾아내면 범인 도망
+         * 범인 추격
+         * 추격은 3인칭으로 변경
+         * 약간 템플런 느낌
+         * 
+         */
+    }
+}
