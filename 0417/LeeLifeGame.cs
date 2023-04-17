df1ccb38045caa6ebe12e1211f3398cced02a37b
현석 이 <inc02179@naver.com>
2023-04-17 20:05:20 +0900

    feat : 이현석 인생게임
    이현석 인생게임

---
+++ 0417/LeeLifeGame.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0417
+{
+    internal class LeeLifeGame
+    {
+        /*
+         * 인생게임
+         * 
+         * 스카이림
+         * 명실상부 오픈월드 RPG게임의 최고봉
+         */
+    }
+}
