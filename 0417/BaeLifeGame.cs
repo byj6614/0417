dd1493be207575c1cf9e316baa9e35c7a055c47e
영준 배 <byj6614@naver.com>
2023-04-17 20:05:39 +0900

    feat : 배영준의 인생게임

---
+++ 0417/BaeLifeGame.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0417
+{
+    internal class BaeLifeGame
+    {
+        //배영준의 인생게임 : 그랜드체이스
+    }
+}
